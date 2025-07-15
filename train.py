0de06194687d8e10911c2fa8783672590a733a7a
sdbds <865105819@qq.com>
2025-07-15 15:55:35 +0800

    update

--- train.py
+++ train.py
@@ -289,11 +289,9 @@ def main(args):
             )
 
         optimizer_class = bnb.optim.AdamW8bit
-    elif args.train.bf16_fused_adam:
-        from bf16_fused_adam import BF16FusedAdamW
-        optimizer_class = BF16FusedAdamW
     else:
-        optimizer_class = torch.optim.AdamW
+        from adamw_bf16 import AdamWBF16
+        optimizer_class = AdamWBF16
 
     log_model_info("transformer", model)
 
