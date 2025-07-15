1e04fddf1e38e573dd4cf16103ce3f072e5e18a7
sdbds <865105819@qq.com>
2025-07-15 14:49:06 +0800

    update bf16 adam

--- train.py
+++ train.py
@@ -289,6 +289,9 @@ def main(args):
             )
 
         optimizer_class = bnb.optim.AdamW8bit
+    elif args.train.bf16_fused_adam:
+        from bf16_fused_adam import BF16FusedAdamW
+        optimizer_class = BF16FusedAdamW
     else:
         optimizer_class = torch.optim.AdamW
 
